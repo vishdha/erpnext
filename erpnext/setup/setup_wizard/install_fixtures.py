72347106e7743fa501d94bb4dd58f039a9795369
Rushabh Mehta <rmehta@gmail.com>
2017-11-16 17:03:52 +0530

    [enhance] Add Issue Type and Opportunity Type masters (#11598)
    * [enhance] added & linked Issue Type & Opportunity Type with opportunity
    
    * [patch] create issue and opportunity type from the custom field if available
    
    * [minor] issue_type and opportunity type should be mandatory
    
    * [patches] removed try catch from the patch
    
    * [fix] patch
    
    * [refactor] cleanup issue/opportunity type

--- erpnext/setup/setup_wizard/install_fixtures.py
+++ erpnext/setup/setup_wizard/install_fixtures.py
@@ -213,6 +213,11 @@ def install(country=None):
 		{'doctype': "Party Type", "party_type": "Employee"},
 		{'doctype': "Party Type", "party_type": "Member"},
 
+		{'doctype': "Opportunity Type", "name": "Hub"},
+		{'doctype': "Opportunity Type", "name": _("Sales")},
+		{'doctype': "Opportunity Type", "name": _("Support")},
+		{'doctype': "Opportunity Type", "name": _("Maintenance")},
+
 		{'doctype': "Project Type", "project_type": "Internal"},
 		{'doctype': "Project Type", "project_type": "External"},
 		{'doctype': "Project Type", "project_type": "Other"},
