ebdd46fdafc50e79113fe9a35e0df3f8f9e8d415
nmimsnikhil <nikhilbachhawat.nmims@gmail.com>
2020-08-05 11:32:40 +0530

    feat: added a fields in production plan item

---
+++ erpnext/patches/v13_0/set_values_in_production_plan_items.py
@@ -0,0 +1,13 @@
+import frappe
+from erpnext.manufacturing.doctype.production_plan.production_plan import get_item_data
+
+
+def execute():
+    frappe.reload_doctype("Production Plan Item")
+    production_plans = frappe.get_all("Production Plan Item", fields=["name", "item_code"])
+    for production_plan in production_plans:
+        data = get_item_data(production_plan.item_code)
+        frappe.db.set_value("Production Plan Item", production_plan.name, "raw_material_cost", data.get("raw_material_cost"), update_modified=False)
+        frappe.db.set_value("Production Plan Item", production_plan.name, "total_operational_cost", data.get("total_operational_cost"), update_modified=False)
+        frappe.db.set_value("Production Plan Item", production_plan.name, "total_operational_hours", data.get("total_operational_hours"), update_modified=False)
+        frappe.db.set_value("Production Plan Item", production_plan.name, "total_workstations", data.get("total_workstations"), update_modified=False)
