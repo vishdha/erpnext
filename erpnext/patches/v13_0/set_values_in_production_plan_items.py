aac91eb7590ec229baa963bd1f227a400196f878
Rohan Bansal <rohan@bloomstack.com>
2020-08-11 18:42:40 +0530

    patch: fix production plan item patch

--- erpnext/patches/v13_0/set_values_in_production_plan_items.py
+++ erpnext/patches/v13_0/set_values_in_production_plan_items.py
@@ -3,13 +3,21 @@ from erpnext.manufacturing.doctype.production_plan.production_plan import get_it
 
 
 def execute():
-    frappe.reload_doctype("Production Plan Item")
-    production_plans = frappe.get_all("Production Plan Item", fields=["name", "item_code","bom_no"])
-    for production_plan in production_plans:
-        if production_plan.get("bom_no"):
-            data = get_item_data(production_plan.item_code)
-            frappe.db.set_value("Production Plan Item", production_plan.name, "raw_material_cost", data.get("raw_material_cost"), update_modified=False)
-            frappe.db.set_value("Production Plan Item", production_plan.name, "total_operational_cost", data.get("total_operational_cost"), update_modified=False)
-            frappe.db.set_value("Production Plan Item", production_plan.name, "total_operational_hours", data.get("total_operational_hours"), update_modified=False)
-            frappe.db.set_value("Production Plan Item", production_plan.name, "total_workstations", data.get("total_workstations"), update_modified=False)
-            
+	frappe.reload_doctype("Production Plan Item")
+	production_plans = frappe.get_all("Production Plan Item", fields=["name", "item_code"])
+
+	for production_plan in production_plans:
+		try:
+			# handle case where an item doesn't have a default BOM
+			data = get_item_data(production_plan.item_code)
+		except frappe.ValidationError:
+			continue
+		else:
+			frappe.db.set_value("Production Plan Item", production_plan.name, "raw_material_cost", data.get(
+				"raw_material_cost"), update_modified=False)
+			frappe.db.set_value("Production Plan Item", production_plan.name, "total_operational_cost", data.get(
+				"total_operational_cost"), update_modified=False)
+			frappe.db.set_value("Production Plan Item", production_plan.name, "total_operational_hours", data.get(
+				"total_operational_hours"), update_modified=False)
+			frappe.db.set_value("Production Plan Item", production_plan.name, "total_workstations", data.get(
+				"total_workstations"), update_modified=False)
