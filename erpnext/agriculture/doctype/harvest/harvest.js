43a1d6981c4bf3faef9d113ab10a177b7a0dc973
vishdha <vishaldhayagude07@gmail.com>
2020-09-08 17:38:05 +0530

    feat(harvest): Stock Entry able to create from Harvest as type harvest

--- erpnext/agriculture/doctype/harvest/harvest.js
+++ erpnext/agriculture/doctype/harvest/harvest.js
@@ -2,7 +2,18 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Harvest', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: (frm) => {
+		if (frm.doc.docstatus === 1) {
+			frm.add_custom_button(__('Stock Entry'), () => frm.trigger('create_stock_entry'), __('Create'));
+			frm.page.set_inner_btn_group_as_primary(__('Create'));
+		}
+	},
+	create_stock_entry: (frm) => {
+		frappe.xcall('erpnext.agriculture.doctype.harvest.harvest.create_stock_entry', {
+			'harvest': frm.doc,
+		}).then(stock_entry => {
+			frappe.model.sync(stock_entry);
+			frappe.set_route("Form", 'Stock Entry', stock_entry.name);
+		});
+	},
 });
--- erpnext/agriculture/doctype/harvest/harvest.py
+++ erpnext/agriculture/doctype/harvest/harvest.py
@@ -3,8 +3,62 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
+import json
 from frappe.model.document import Document
 
 class Harvest(Document):
 	pass
+
+@frappe.whitelist()
+def create_stock_entry(harvest):
+	harvest = frappe.get_doc(json.loads(harvest))
+	if stock_entry_exists(harvest.get('name')):
+		return frappe.msgprint(_('Stock Entry has been already created against this harvest'))
+
+	stock_entry = frappe.new_doc('Stock Entry')
+	stock_entry.harvest = harvest.get('name')
+	stock_entry.purpose = 'Material Receipt',
+	# stock_entry.set_stock_entry_type()
+
+	if harvest.get('strain'):
+		stock_entry = update_stock_entry_based_on_strain(harvest, stock_entry)
+
+	stock_entry.set_incoming_rate()
+	stock_entry.set_actual_qty()
+	stock_entry.calculate_rate_and_amount(update_finished_item_rate=False)
+
+	return stock_entry.as_dict()
+
+
+def stock_entry_exists(harvest_name):
+	return frappe.db.exists('Stock Entry', {
+		'harvest': harvest_name
+	})
+
+
+def update_stock_entry_based_on_strain(harvest, stock_entry):
+	strain = frappe.get_doc("Strain", harvest.get('strain'))
+
+	stock_entry.to_warehouse = strain.target_warehouse
+
+	for location in strain.produced_items:
+		item = frappe._dict()
+		update_common_item_properties(item, location)
+		item.t_warehouse = strain.target_warehouse
+
+		stock_entry.append('items', item)
+
+	for location in strain.byproducts:
+		item = frappe._dict()
+		update_common_item_properties(item, location)
+		item.t_warehouse = strain.target_warehouse
+
+		stock_entry.append('items', item)
+
+	return stock_entry
+
+
+def update_common_item_properties(item, location):
+	item.item_code = location.item_code
+
