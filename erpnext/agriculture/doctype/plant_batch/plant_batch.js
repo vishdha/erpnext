7e776f018b1c94ca1417a1ffd68fe8d7e950a313
Rohan <Alchez@users.noreply.github.com>
2020-09-23 18:43:02 +0530

    feat: add Harvest Plant table (#588)

--- erpnext/agriculture/doctype/plant_batch/plant_batch.js
+++ erpnext/agriculture/doctype/plant_batch/plant_batch.js
@@ -4,6 +4,14 @@
 frappe.ui.form.on('Plant Batch', {
 	setup: function (frm) {
 		frm.make_methods = {
+			'Plant': () => frappe.model.open_mapped_doc({
+				method: "erpnext.agriculture.doctype.plant_batch.plant_batch.make_plant",
+				frm: frm
+			}),
+			'Harvest': () => frappe.model.open_mapped_doc({
+				method: "erpnext.agriculture.doctype.plant_batch.plant_batch.make_harvest",
+				frm: frm
+			}),
 			'Destroyed Plant Log': () => { frm.trigger("destroy_plant_batch"); }
 		};
 	},
@@ -15,13 +23,13 @@ frappe.ui.form.on('Plant Batch', {
 		frappe.realtime.on("List of Linked Docs", (output) => {
 			let analysis_doctypes = ['Soil Texture', 'Plant Analysis', 'Soil Analysis'];
 			let analysis_doctypes_docs = ['soil_texture', 'plant_analysis', 'soil_analysis'];
-			let obj_to_append = {soil_analysis: [], soil_texture: [], plant_analysis: []};
-			output['Location'].forEach( (land_doc) => {
-				analysis_doctypes.forEach( (doctype) => {
-					output[doctype].forEach( (analysis_doc) => {
+			let obj_to_append = { soil_analysis: [], soil_texture: [], plant_analysis: [] };
+			output['Location'].forEach((land_doc) => {
+				analysis_doctypes.forEach((doctype) => {
+					output[doctype].forEach((analysis_doc) => {
 						let point_to_be_tested = JSON.parse(analysis_doc.location).features[0].geometry.coordinates;
 						let poly_of_land = JSON.parse(land_doc.location).features[0].geometry.coordinates[0];
-						if (is_in_land_unit(point_to_be_tested, poly_of_land)){
+						if (is_in_land_unit(point_to_be_tested, poly_of_land)) {
 							obj_to_append[analysis_doctypes_docs[analysis_doctypes.indexOf(doctype)]].push(analysis_doc.name);
 						}
 					});
@@ -31,29 +39,8 @@ frappe.ui.form.on('Plant Batch', {
 				obj_to_append: obj_to_append
 			});
 		});
-
-		if (!frm.is_new()) {
-			frm.add_custom_button(__("Plant"), () => {
-				frappe.model.open_mapped_doc({
-					method: "erpnext.agriculture.doctype.plant_batch.plant_batch.make_plant",
-					frm: frm
-				});
-			}, __("Create"));
-			frm.add_custom_button(__("Additive Log"), () => {
-				frappe.model.open_mapped_doc({
-					method: "erpnext.agriculture.doctype.plant_batch.plant_batch.make_additive_log",
-					frm: frm
-				});
-			}, __("Create"));
-			frm.add_custom_button(__("Disease Diagnosis"), () => {
-				frappe.model.open_mapped_doc({
-					method: "erpnext.agriculture.doctype.plant_batch.plant_batch.make_disease_diagnosis",
-					frm: frm
-				});
-			}, __("Create"));
-		}
-
 	},
+
 	destroy_plant_batch: (frm) => {
 		frappe.prompt([{
 			fieldname: 'destroy_count',
