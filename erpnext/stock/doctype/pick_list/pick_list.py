f5f1ad1e382ad826dbdb29bee3bebf9400a3b235
bhattdevarsh <58166671+bhattdevarsh@users.noreply.github.com>
2020-10-13 11:25:05 +0530

    Update erpnext/stock/doctype/pick_list/pick_list.py
    Co-authored-by: Himanshu <himanshuwarekar@yahoo.com>

--- erpnext/stock/doctype/pick_list/pick_list.py
+++ erpnext/stock/doctype/pick_list/pick_list.py
@@ -395,6 +395,7 @@ def create_delivery_note(source_name, target_doc=None):
 			else [location, item_table_mapper_without_so]
 
 		dn_item = map_child_doc(source_doc, delivery_note, table_mapper)
+		coa_batch = None
 		if location.source_package_tag:
 			coa_batch = frappe.db.get_value("Package Tag", location.source_package_tag, "coa_batch_no")
 			
