984d83eb3a80020ae5d968022de8dabb92b1dc15
Myuddin khatri <myuddin@digithinkit.com>
2020-10-20 12:51:57 +0530

    fix: changed flow to finish and unfinish harvest

--- erpnext/agriculture/doctype/harvest/harvest.js
+++ erpnext/agriculture/doctype/harvest/harvest.js
@@ -2,31 +2,6 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Harvest', {
-	refresh: function (frm) {
-		if (frm.doc.docstatus === 1) {
-			frm.add_custom_button(__("Finish"), () => {
-				frappe.call({
-					method: 'erpnext.agriculture.doctype.harvest.harvest.finish_harvest',
-					args: { 'harvest': frm.doc.name },
-					callback: (r) => {
-						if (r.message) {
-							frm.reload_doc();
-						}
-					}
-				})
-			});
-
-			frm.add_custom_button(__("UnFinish"), () => {
-				frappe.call({
-					method: 'erpnext.agriculture.doctype.harvest.harvest.unfinish_harvest',
-					args: { 'harvest': frm.doc.name },
-					callback: (r) => {
-						if (r.message) {
-							frm.reload_doc();
-						}
-					}
-				})
-			});
-		}
-	}
+	// refresh: function (frm) {
+	// }
 });
--- erpnext/agriculture/doctype/harvest/harvest.py
+++ erpnext/agriculture/doctype/harvest/harvest.py
@@ -3,21 +3,8 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-import frappe
+# import frappe
 from frappe.model.document import Document
-from bloomstack_core.bloomtrace import make_integration_request
 
 class Harvest(Document):
 	pass
-
-@frappe.whitelist()
-def finish_harvest(harvest):
-	frappe.db.set_value("Harvest", harvest, "is_finished", 1)
-	make_integration_request("Harvest", harvest)
-	return True
-
-@frappe.whitelist()
-def unfinish_harvest(harvest):
-	frappe.db.set_value("Harvest", harvest, "is_finished", 0)
-	make_integration_request("Harvest", harvest)
-	return True
