vishdha/erpnext

commit dee1d286885c34116230d83085fc2c17adda4efb

feat(stock_entry_type): Harvest Stock Entry type and patch added

erpnext/setup/setup_wizard/operations/install_fixtures.py
@@ -95,6 +95,7 @@ def install(country=None):
 		{'doctype': 'Stock Entry Type', 'name': 'Material Consumption for Manufacture', 'purpose': 'Material Consumption for Manufacture'},
 		{'doctype': 'Stock Entry Type', 'name': 'Send to Warehouse', 'purpose': 'Send to Warehouse'},
 		{'doctype': 'Stock Entry Type', 'name': 'Receive at Warehouse', 'purpose': 'Receive at Warehouse'},
+		{'doctype': 'Stock Entry Type', 'name': 'Harvest', 'purpose': 'Material Receipt'}
 
 		# Designation
 		{'doctype': 'Designation', 'designation_name': _('CEO')},

erpnext/patches/v13_0/harvest_stock_entry_type_added.py
@@ -1,0 +1,24 @@
+from __future__ import unicode_literals
+import frappe
+
+def execute():
+	create_stock_entry_types()
+
+def create_stock_entry_types():
+	frappe.reload_doc('stock', 'doctype', 'stock_entry_type')
+	frappe.reload_doc('stock', 'doctype', 'stock_entry')
+
+	for purpose in ["Material Issue", "Material Receipt", "Material Transfer",
+		"Material Transfer for Manufacture", "Material Consumption for Manufacture", "Manufacture",
+		"Repack", "Send to Subcontractor", "Send to Warehouse", "Receive at Warehouse", "Harvest"]:
+	   
+		ste_type = frappe.get_doc({
+			'doctype': 'Stock Entry Type',
+			'name': purpose,
+			'purpose': "Material Receipt" if purpose == "Harvest" else purpose
+		})
+
+		try:
+			ste_type.insert()
+		except frappe.DuplicateEntryError:
+			pass

commit 1c44fd936c9ca59f5319ab6845769c57278db142

fix: syntax error
introduced by https://github.com/Bloomstack/erpnext/commit/dee1d286885c34116230d83085fc2c17adda4efb#diff-ead2c1470000063671725e11fa7facf1c3600ec11171a63120510af9ae09a045R98

erpnext/setup/setup_wizard/operations/install_fixtures.py
@@ -95,7 +95,7 @@ def install(country=None):
 		{'doctype': 'Stock Entry Type', 'name': 'Material Consumption for Manufacture', 'purpose': 'Material Consumption for Manufacture'},
 		{'doctype': 'Stock Entry Type', 'name': 'Send to Warehouse', 'purpose': 'Send to Warehouse'},
 		{'doctype': 'Stock Entry Type', 'name': 'Receive at Warehouse', 'purpose': 'Receive at Warehouse'},
-		{'doctype': 'Stock Entry Type', 'name': 'Harvest', 'purpose': 'Material Receipt'}
+		{'doctype': 'Stock Entry Type', 'name': 'Harvest', 'purpose': 'Material Receipt'},
 
 		# Designation
 		{'doctype': 'Designation', 'designation_name': _('CEO')},