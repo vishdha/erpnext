c8307e58e1102346e71f36f1e42b79b67bb5ab7d
bhattdevarsh <58166671+bhattdevarsh@users.noreply.github.com>
2020-10-27 11:33:56 +0530

    Update erpnext/stock/doctype/delivery_note/delivery_note_list.js
    Co-authored-by: Himanshu <himanshuwarekar@yahoo.com>

--- erpnext/stock/doctype/delivery_note/delivery_note_list.js
+++ erpnext/stock/doctype/delivery_note/delivery_note_list.js
@@ -2,7 +2,7 @@ frappe.listview_settings['Delivery Note'] = {
 	add_fields: ["customer", "customer_name", "base_grand_total", "per_installed", "per_billed",
 		"transporter_name", "grand_total", "is_return", "status", "currency"],
 	get_indicator: function(doc) {
-		if (cint(doc.is_return) == 1) {
+		if (cint(doc.is_return) === 1) {
 			return [__("Return"), "darkgrey", "is_return,=,Yes"];
 		} else if (doc.status === "Closed") {
 			return [__("Closed"), "green", "status,=,Closed"];
