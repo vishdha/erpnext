226e634a28dfb81c107d27c1e79d0d5962e34c85
bhattdevarsh <58166671+bhattdevarsh@users.noreply.github.com>
2020-10-27 11:33:27 +0530

    Update erpnext/patches/v13_0/update_delivery_note_status_to_in_transit.py
    Co-authored-by: Himanshu <himanshuwarekar@yahoo.com>

--- erpnext/patches/v13_0/update_delivery_note_status_to_in_transit.py
+++ erpnext/patches/v13_0/update_delivery_note_status_to_in_transit.py
@@ -2,8 +2,9 @@ import frappe
 
 
 def execute():
-    frappe.reload_doc("stock", "doctype", "delivery_note", force=True)
     delivery_notes = frappe.db.get_all(
         "Delivery Note", {"status": "Out for Delivery"}, "name")
+    frappe.reload_doc("stock", "doctype", "delivery_note", force=True)
+    
     for delivery_note in delivery_notes:
-        frappe.db.set_value("Delivery Note", delivery_note.name, {'status': 'In Transit'})
+        frappe.db.set_value("Delivery Note", delivery_note.name, "status", "In Transit")
