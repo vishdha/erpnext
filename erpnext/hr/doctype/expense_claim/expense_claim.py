b4e98b399a2571bb65ff789d0cb1026f61b5b732
nmimsnikhil <nikhilbachhawat.nmims@gmail.com>
2020-11-10 14:34:02 +0530

    feat:api to create expense claim

--- erpnext/hr/doctype/expense_claim/expense_claim.py
+++ erpnext/hr/doctype/expense_claim/expense_claim.py
@@ -360,3 +360,29 @@ def get_expense_claim(
 	)
 
 	return expense_claim
+
+@frappe.whitelist()
+def new_expense_claim(subject, email, expense_date, e_type, amount, status):
+	# fetch employee code from email
+	if frappe.get_value("Employee",{ "prefered_email": email }):
+		employee_name = frappe.get_value("Employee",{ "prefered_email": email })
+
+	elif frappe.get_value("Employee",{ "company_email": email }):
+		employee_name = frappe.get_value("Employee",{ "company_email": email })
+
+	else:
+		employee_name = frappe.get_value("Employee",{ "personal_email": email })
+
+	if not employee_name:
+		frappe.throw("No Employee Found")
+
+	doc = frappe.new_doc("Expense Claim")
+	doc.employee = employee_name
+	doc.payable_account = frappe.get_value('Company', frappe.get_value("Employee", employee_name, "company"), 'default_payable_account')
+	doc.approval_status = status
+	child_doc = doc.append('expenses', {})
+	child_doc.expense_date = expense_date
+	child_doc.expense_type = e_type
+	child_doc.amount = amount
+	child_doc.description = subject
+	doc.save()
