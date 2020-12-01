4fd05ee90a292126110f54fe48ff8e7003495fb3
sahil28297 <37302950+sahil28297@users.noreply.github.com>
2020-12-01 12:09:26 +0530

    fix: throw error in correct sequence during card validation (#773)

--- erpnext/templates/includes/integrations/authorizenet_checkout.js
+++ erpnext/templates/includes/integrations/authorizenet_checkout.js
@@ -24,16 +24,16 @@ $('#submit').on("click", function(e) {
 		frappe.throw(__("Card Number is Invalid."));
 	}
 
+	if(cardNumber.length < 13 || cardNumber.length > 16){
+		frappe.throw(__("Card Number length should be between 13 and 16 characters"));
+	}
+
 	if(expirationMonth === "00" || expirationMonth.length !== 2 || expirationYear === "0000" || expirationYear.length !== 4){
 		frappe.throw(__("Card Expiration Date is invalid"));
 	}
 
 	if(cardCode.length < 3 || cardCode.length > 4){
 		frappe.throw(__("Card Code length should be between 3 and 4 characters"));
-	}
-
-	if(cardNumber.length < 13 || cardNumber.length > 16){
-		frappe.throw(__("Card number length should be between 13 and 16 characters"));
 	}
 
 	$('#submit').prop('disabled', true);
@@ -69,7 +69,7 @@ $('input[data-validation="digit"]')
 		return (event.charCode !== 8 && event.charCode === 0 || (event.charCode >= 48 && event.charCode <= 57));
 	});
 
-$('#card-number').on('keyup', function () {
+$('#card-number').on('keydown', function () {
 	var val = $(this).val();
 	val = val.replace(/\s/g, '');
 	var newval = val.match(/.{1,4}/g).join(" ");
