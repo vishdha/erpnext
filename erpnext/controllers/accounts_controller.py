c9b367f3073b91a943b9d2215c125abeb6b41e31
vishdha <vishaldhayagude07@gmail.com>
2020-12-11 13:38:36 +0530

    fix: allocated amount based on original advance paid amount added

--- erpnext/controllers/accounts_controller.py
+++ erpnext/controllers/accounts_controller.py
@@ -478,11 +478,12 @@ class AccountsController(TransactionBase):
 
 		self.set("advances", [])
 		advance_allocated = 0
-		print("-------------------------------------------------------------------", res)
 		for d in res:
 			if d.against_order:
 				if self.allocate_advances_based_on_quantities:
-					allocated_amount = flt(self.total_qty / 5000) * d.amount
+					# formula for calculating allocated amount = (Fullfilled qty / Order Qty) * original_advance_paid_amount
+					order_qty = frappe.db.get_value("Sales Order", filters={"name":d.against_order}, fieldname=["total_qty"])
+					allocated_amount = flt(self.total_qty / order_qty) * d.total_allocated_amount
 				else:
 					allocated_amount = flt(d.amount)
 			else:
@@ -1009,7 +1010,7 @@ def get_advance_journal_entries(party_type, party, party_account, amount_field,
 		select
 			"Journal Entry" as reference_type, t1.name as reference_name,
 			t1.remark as remarks, t2.{0} as amount, t2.name as reference_row,
-			t2.reference_name as against_order
+			t2.reference_name as against_order, t1.total_amount as total_allocated_amount
 		from
 			`tabJournal Entry` t1, `tabJournal Entry Account` t2
 		where
@@ -1042,7 +1043,7 @@ def get_advance_payment_entries(party_type, party, party_account, order_doctype,
 			select
 				"Payment Entry" as reference_type, t1.name as reference_name,
 				t1.remarks, t2.allocated_amount as amount, t2.name as reference_row,
-				t2.reference_name as against_order, t1.posting_date
+				t2.reference_name as against_order, t1.posting_date, t1.total_allocated_amount
 			from `tabPayment Entry` t1, `tabPayment Entry Reference` t2
 			where
 				t1.name = t2.parent and t1.{0} = %s and t1.payment_type = %s
