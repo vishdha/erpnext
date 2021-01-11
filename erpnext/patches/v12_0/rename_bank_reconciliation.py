0662f9e069e413099670ef3073eaf54729555c5f
vishdha <vishaldhayagude07@gmail.com>
2021-01-11 14:58:25 +0530

    fix: minor changes added

--- erpnext/patches/v12_0/rename_bank_reconciliation.py
+++ erpnext/patches/v12_0/rename_bank_reconciliation.py
@@ -11,3 +11,11 @@ def execute():
 
 		frappe.rename_doc('DocType', 'Bank Reconciliation Detail', 'Bank Clearance Detail', force=True)
 		frappe.reload_doc('Accounts', 'doctype', 'Bank Clearance Detail')
+
+	_rename_single_field(doctype = "Bank Clearance", old_name = "bank_account" , new_name = "account")
+	_rename_single_field(doctype = "Bank Clearance", old_name = "bank_account_no", new_name = "bank_account")
+
+def _rename_single_field(**kwargs):
+	count = frappe.db.sql("SELECT COUNT(*) FROM tabSingles WHERE doctype='{doctype}' AND field='{new_name}';".format(**kwargs))[0][0] #nosec
+	if count == 0:
+		frappe.db.sql("UPDATE tabSingles SET field='{new_name}' WHERE doctype='{doctype}' AND field='{old_name}';".format(**kwargs)) #nosec
