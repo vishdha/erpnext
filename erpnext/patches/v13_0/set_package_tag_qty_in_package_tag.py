79201335ce63e736b5e2fbe592dae1b2138373d8
Myuddin khatri <myuddin@digithinkit.com>
2021-01-11 14:35:12 +0530

    fix: changed get_value to sql query in patch to update package_tag_qty

--- erpnext/patches/v13_0/set_package_tag_qty_in_package_tag.py
+++ erpnext/patches/v13_0/set_package_tag_qty_in_package_tag.py
@@ -5,6 +5,21 @@ def execute():
 	package_tags = frappe.get_all("Package Tag")
 
 	for package_tag in package_tags:
-		package_tag_qty = frappe.db.get_value("Stock Ledger Entry", {"docstatus": 1, "package_tag": package_tag.name}, "sum(actual_qty)")
-		if package_tag_qty:
-			frappe.db.set_value("Package Tag", package_tag.name, "package_tag_qty", package_tag_qty)
+		package_tag_qty = frappe.db.sql("""
+			SELECT
+				sum(actual_qty) as qty
+			FROM
+				`tabStock Ledger Entry` as sle
+			WHERE
+				sle.package_tag = (%s)
+		""",(package_tag.name), as_dict=1)
+
+		if package_tag_qty[0].qty:
+			frappe.db.sql("""
+				UPDATE
+					`tabPackage Tag`
+				SET
+					package_tag_qty = %s
+				WHERE
+					name = %s
+			""",(package_tag_qty[0].qty, package_tag.name))
