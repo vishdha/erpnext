f384f75540d198605f805b8b84fdf87a97121357
Hitensh Kharva <56474017+hitenshKharva@users.noreply.github.com>
2021-02-22 11:13:25 +0530

    fix: fetch manufacturing type from work_order in stock entry (#1035)

--- erpnext/manufacturing/doctype/work_order/work_order.py
+++ erpnext/manufacturing/doctype/work_order/work_order.py
@@ -694,6 +694,7 @@ def make_stock_entry(work_order_id, purpose, qty=None):
 	stock_entry.company = work_order.company
 	stock_entry.from_bom = 1
 	stock_entry.bom_no = work_order.bom_no
+	stock_entry.manufacturing_type = work_order.manufacturing_type
 	stock_entry.use_multi_level_bom = work_order.use_multi_level_bom
 	stock_entry.fg_completed_qty = qty or (flt(work_order.qty) - flt(work_order.produced_qty))
 	if work_order.bom_no:
