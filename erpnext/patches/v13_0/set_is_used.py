7540677bca2823c10e5499a00990c350db87eb02
Himanshu <himanshuwarekar@yahoo.com>
2021-03-23 10:32:46 +0530

    fix: add "has_transaction" in package tags (#1107)
    * fix: add has transaction in package tags
    
    * fix: rename has_transactions to is_used

---
+++ erpnext/patches/v13_0/set_is_used.py
@@ -0,0 +1,9 @@
+import frappe
+
+def execute():
+	"""
+		Sets IS Used in Package Tag if there are SLEs
+	"""
+	for package_tag in frappe.get_all("Package Tag"):
+		if frappe.db.exists("Stock Ledger Entry", {"package_tag": package_tag.name}):
+			frappe.db.set_value("Package Tag", package_tag.name, "is_used", 1)
