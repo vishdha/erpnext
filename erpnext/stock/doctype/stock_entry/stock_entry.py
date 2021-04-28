a7a6b4176ba7b31801e65a9c6d71b79f1d217040
Myuddin Khatri <53251406+MyuddinKhatri@users.noreply.github.com>
2021-04-28 14:34:15 +0530

    feat: added customer and supplier information to Batch (#1187)
    * feat: added customer and supplier information to Batch
    
    * feat: added customer providing item and supplier information to Batch
    
    * fix: made Supplier and Customer Providing Item field as read only
    
    * fix: made Supplier and Customer Providing Item field as link field
    
    * fix: changed field customer providing item to provided by customer in batch
    
    * fix: changed field customer providing item to provided by customer in batch

--- erpnext/stock/doctype/stock_entry/stock_entry.py
+++ erpnext/stock/doctype/stock_entry/stock_entry.py
@@ -100,6 +100,7 @@ class StockEntry(StockController):
 			self.update_so_in_serial_number()
 		self.update_package_tag()
 		self.update_package_tag_is_used()
+		self.update_batch_with_customer_provided_item()
 
 	def on_cancel(self):
 
@@ -688,6 +689,16 @@ class StockEntry(StockController):
 			sl_entries.reverse()
 
 		self.make_sl_entries(sl_entries, self.amended_from and 'Yes' or 'No')
+
+	def update_batch_with_customer_provided_item(self):
+		if self.stock_entry_type == "Material Receipt":
+			for item in self.items:
+				item_data = frappe.db.get_values("Item", item.item_code, ["is_customer_provided_item", "has_batch_no"], as_dict=1)
+				if item_data[0].is_customer_provided_item and item_data[0].has_batch_no and item.batch_no and item.material_request:
+					batch_provided_by_customer = frappe.db.get_value("Batch", item.batch_no, "provided_by_customer")
+					if not batch_provided_by_customer:
+						customer = frappe.db.get_value("Material Request", item.material_request, "customer")
+						frappe.db.set_value("Batch", item.batch_no, "provided_by_customer", customer)
 
 	def get_gl_entries(self, warehouse_account):
 		gl_entries = super(StockEntry, self).get_gl_entries(warehouse_account)
