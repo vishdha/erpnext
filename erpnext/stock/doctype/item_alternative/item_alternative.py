dfc5cdcf1b51e07f0f2a9548c89d41b50c5ca7d9
vishdha <vishaldhayagude07@gmail.com>
2021-05-19 18:49:12 +0530

    fix: index error fixed for item_alternative query

--- erpnext/stock/doctype/item_alternative/item_alternative.py
+++ erpnext/stock/doctype/item_alternative/item_alternative.py
@@ -43,13 +43,26 @@ class ItemAlternative(Document):
 			frappe.throw(_("Already record exists for the item {0}".format(self.item_code)))
 
 def get_alternative_items(doctype, txt, searchfield, start, page_len, filters):
-	return frappe.db.sql(""" (select alternative_item_code from `tabItem Alternative`
-			where item_code = %(item_code)s and alternative_item_code like %(txt)s)
+	return frappe.db.sql("""
+		(select
+			alternative_item_code, alternative_item_name
+		from
+			`tabItem Alternative`
+		where
+			item_code = %(item_code)s and alternative_item_code like %(txt)s)
 		union
-			(select item_code from `tabItem Alternative`
-			where alternative_item_code = %(item_code)s and item_code like %(txt)s
-			and two_way = 1) limit {0}, {1}
-		""".format(start, page_len), {
-			"item_code": filters.get('item_code'),
-			"txt": '%' + txt + '%'
+			(select
+				item_code, item_name
+			from
+				`tabItem Alternative`
+			where
+				alternative_item_code = %(item_code)s and item_code like %(txt)s and two_way = 1)
+		limit %(start)s, %(page_len)s""".format(**{
+			'key': searchfield
+		}), {
+			'txt': "%%%s%%" % txt,
+			'item_code': filters.get('item_code'),
+			'_txt': txt.replace("%", ""),
+			'start': start,
+			'page_len': page_len
 		})
