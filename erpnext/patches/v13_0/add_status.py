4b7dd243834a27133232901ab9efbfd8166a02c0
vishdha <vishaldhayagude07@gmail.com>
2021-06-04 11:22:22 +0530

    fix: status updated

--- erpnext/patches/v13_0/add_status.py
+++ erpnext/patches/v13_0/add_status.py
@@ -4,4 +4,6 @@ def execute():
 	from erpnext.setup.setup_wizard.operations.install_fixtures import add_status_data
 
 	frappe.reload_doc("setup", "doctype", "Status")
-	add_status_data()
+
+	if not frappe.db.a_row_exists("Status"):
+		add_status_data()
