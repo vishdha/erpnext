78bee8ebab909a5ee281fe1b203122ffb9b3f4dd
Myuddin Khatri <53251406+MyuddinKhatri@users.noreply.github.com>
2021-06-08 12:28:56 +0530

    fix: applied filter for batch_no, batch_qty > 0 in product recall (#1361)
    * fix: applied filter for batch_no, batch_qty > 0 in product recall
    
    * Update erpnext/stock/doctype/batch/batch.py
    
    Co-authored-by: sahil28297 <37302950+sahil28297@users.noreply.github.com>

--- erpnext/stock/doctype/product_recall/product_recall.js
+++ erpnext/stock/doctype/product_recall/product_recall.js
@@ -5,4 +5,13 @@ frappe.ui.form.on('Product Recall', {
 	// refresh: function(frm) {
 
 	// }
+	onload: function(frm) {
+		frm.set_query('batch_no', function(doc) {
+			return {
+				filters: {
+					"batch_qty": [">", 0]
+				}
+			};
+		});
+	},
 });
