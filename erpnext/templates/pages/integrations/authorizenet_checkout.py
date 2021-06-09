fd56763bf92660bd8388c5e4e335edb43a3bb496
Felipe Orellana <1656249+DeveloperMetal@users.noreply.github.com>
2021-06-09 01:18:41 -0400

    fix(AuthorizeNet): Fixes issue where payment data was incorrectly encoded into json (#1370)

--- erpnext/templates/pages/integrations/authorizenet_checkout.py
+++ erpnext/templates/pages/integrations/authorizenet_checkout.py
@@ -2,11 +2,13 @@
 # License: GNU General Public License v3. See license.txt
 
 import frappe
+import json
 from frappe.utils import fmt_money
 
 
 def get_context(context):
 	context.no_cache = 1
 	payment_context = dict(frappe.local.request.args)
-	context.payment_context = payment_context
+	context.payment_context = json.dumps(payment_context)
+	context["description"] = payment_context.get("description") or ""
 	context["amount"] = fmt_money(amount=payment_context.get('amount'), currency=payment_context.get('currency'))
